56b47ea77dc9f33fe01723171f3dedec609f0a2f
anasanzari <anasanzari@gmail.com>
2018-01-23 12:42:38 +0530

    init

---
+++ spec/atom-snake-copy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSnakeCopyView from '../lib/atom-snake-copy-view';
+
+describe('AtomSnakeCopyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
